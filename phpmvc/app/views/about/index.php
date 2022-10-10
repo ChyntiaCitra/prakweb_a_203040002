3e22035d322307271f8f1344f944639edb4f494b
ChyntiaCitra <2030400002@mail.com.unpas.ac.id>
2022-10-10 22:11:29 +0700

    menyelesaikan video 6

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,2 +1,6 @@
-<h1>About Me</h1>
+<div class="container">
+
+<h1 class="mt-4">About Me</h1>
+<img src="<?= BASEURL; ?>/img/picture.jpg" alt="Chyntia Citra" width="200">
 <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan'] ?></p>
+</div>
--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,1 +1,12 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+<div class="container">
+  <div class="jumbotron mt-5">
+    <h1>Selamat Datang di Website Saya.</h1>
+    <p class="lead">Halo nama saya <?= $data['nama'] ?></p>
+    <hr class="my-4">
+    <p>Saya adalah seorang mahasiswa Universitas Pasundan Semester 5.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Lihat detail</a>
+  </div>
+</div>
+
+</div>
